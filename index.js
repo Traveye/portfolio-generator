c47a1df35d2a21f5f0e92ba209caac878310ec5a
DBENSAN <dftb97@gmail.com>
2023-01-23 12:17:37 -0800

    adds prompt and file write

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-var html = 
-`
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hello Bulma!</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-  </head>
-  <body>
-  <section class="section">
-    <div class="container">
-      <h1 class="title">
-        Hello my name is ${'name'}
-      </h1>
-      <p class="subtitle">
-        I live in ${'location'}. ${'bio'}. You can find out more about me here ${'linked'} and here ${'github'}. 
-      </p>
-    </div>
-  </section>
-  </body>
-</html>
-`
+inquirer
+.prompt([
+  {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Where do you live?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'Tell us about yourself.',
+      name: 'bio',
+    },
+    {
+      type: 'input',
+      message: 'What is your LinkedIn URL?',
+      name: 'linked',
+    },
+    {
+      type: 'input',
+      message: 'What is your GitHub URL?',
+      name: 'github',
+    },
+  ])
+
+  .then((answers) => {
+    console.log(answers)
+    var html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Hello Bulma!</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    </head>
+    <body>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          Hello my name is ${answers.name}
+        </h1>
+        <p class="subtitle">
+          I live in ${answers.location}. 
+          ${answers.bio}. 
+          
+          You can find out more about me here ${answers.linked} and here ${answers.github}. 
+
+        </p>
+      </div>
+    </section>
+    </body>
+  </html>
+  `
+    fs.appendFile('profile.html', html, (err) => err ? console.log(err) : console.log('Sucess'))
+  })
+
+  
+  
